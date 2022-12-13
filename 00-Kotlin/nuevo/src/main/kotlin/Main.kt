1713560c5f7ed2d64d32e67a6a9cb96a3750a718
Andrea Garces <andrea.garces@epn.edu.ec>
2022-12-13 10:53:36 -0500

    Operadores 2

--- 00-Kotlin/nuevo/src/main/kotlin/Main.kt
+++ 00-Kotlin/nuevo/src/main/kotlin/Main.kt
@@ -90,6 +90,47 @@ fun main(){
     val respuestaMapDos = arregloDinamico.map { it + 15 }
 
     println(respuestaMapDos)
+
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresion (TRUE o FALSE)
+    // 2) Nuevo arreglo filtrado
+
+    val respuestaFilter: List<Int> = arregloDinamico.filter { valorActual: Int ->
+        val mayoresACinco: Boolean = valorActual > 5 // Expresion Condicion
+        return@filter mayoresACinco
+    }
+    val respuestaFilterDos = arregloDinamico.filter { it <= 5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    // OR AND
+    // OR -> ANY (Alguno cumple?)
+    // AND -> ALL (Todos cumplen?)
+
+    val respuestaAny: Boolean = arregloDinamico.any{ valorActual: Int ->
+        return@any (valorActual > 5)
+    }
+    println(respuestaAny) // true
+
+    val respuestaAll: Boolean = arregloDinamico.all { valorActual: Int ->
+        return@all(valorActual > 5)
+    }
+    println(respuestaAll) // false
+
+    // REDUCE -> Valor acumulado
+    // Valor acumulado = 0 (Siempre 0 en lenguaje Kotlin)
+    // [1, 2, 3, 4, 5] -> Sume todos los valores del arreglo
+    // valorIteracion1 = valorEmpieza + 1 = 0 + 1 = 1
+    // valorIteracion2 = valorIteracion1 + 2 = 1 + 2 = 3 -> Iteracion2
+    // valorIteracion3 = valorIteracion2 + 3 = 3 + 3 = 6 -> Iteracion3
+    // valorIteracion4 = valorIteracion3 + 4 = 6 + 4 = 10 -> Iteracion4
+    // valorIteracion5 = valorIteracion4 + 5 = 10 + 5 = 15 -> Iteracion5
+
+    val respuestaReduce: Int = arregloDinamico.reduce{ // acumulado = 0 -> SIEMPRE EMPIEZA EN 0
+        acumulado: Int, valorActual: Int ->
+        return@reduce (acumulado + valorActual) // -> Logica negocio
+    }
+    println(respuestaReduce) // 78
 }
 
 // Unit == void
